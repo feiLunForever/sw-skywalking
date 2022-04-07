e47f7d553e2c0a928a526f146d8d7e2014d5d766
trotyzyq <trotyzyq@qq.com>
2022-04-07 17:36:57 +0800

    添加swagger插件模块

---
+++ swcourse-swagger/src/main/java/com/swcourse/swagger/SwCourseSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.swcourse.swagger;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwCourseSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwCourseSwaggerApplication.class, args);
+    }
+}
