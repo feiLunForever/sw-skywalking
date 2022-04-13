c4fe105566e661b5bf66277d26ae673c8fab111b
trotyzyq <trotyzyq@qq.com>
2022-04-13 19:41:44 +0800

    添加swagger模块

--- swcourse-swagger/src/main/java/com/swcourse/swagger/SwaggerController.java
+++ swcourse-swagger/src/main/java/com/swcourse/swagger/SwaggerController.java
@@ -1,11 +1,29 @@
 package com.swcourse.swagger;
 
+import com.fasterxml.classmate.ResolvedType;
+import com.google.common.base.Optional;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.condition.NameValueExpression;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.RequestHandlerKey;
+import springfox.documentation.service.ResolvedMethodParameter;
+import springfox.documentation.spring.web.WebMvcRequestHandler;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author your name or email
@@ -21,6 +39,21 @@ public class SwaggerController {
     @ApiOperation("测试1")
     @GetMapping("/test")
     public String test(){
+//        WebMvcRequestHandler webMvcRequestHandler = new WebMvcRequestHandler();
+//        // 类: webMvcRequestHandler.getHandlerMethod().getBeanType().getName();
+//        // Tag:
+//        Annotation[] annotations =  webMvcRequestHandler.getHandlerMethod().getBeanType().getAnnotations();
+//        Api api = null;
+//        String[] tags = api.tags();
+//
+//        // apiOperation
+//        Annotation[] annotations2 =  webMvcRequestHandler.getHandlerMethod().getMethod().getAnnotations();
+//        ApiOperation apiOperation = null;
+//        String value = apiOperation.value();
+//
+//        // 路径
+//        new ArrayList<>(webMvcRequestHandler.getRequestMapping().getPatternsCondition().getPatterns()).get(0);
         return "success";
+
     }
 }
