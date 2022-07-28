5403baeecc5fdb815a3d2cbe233babf0a87f762c
zhangyuqiang <yuqiang.zhangyq@dtzhejiang.com>
2022-07-28 15:19:44 +0800

    测试项目地址

--- swcourse-swagger/src/main/java/com/swcourse/swagger/PunchRecordMapper.java
+++ swcourse-swagger/src/main/java/com/swcourse/swagger/PunchRecordMapper.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
- * 打卡记录表 Mapper 接口
+ * 打卡记录表 Mapper  接口
  * @author zhangyuqiang
  * @since 2021-11-26
  */
