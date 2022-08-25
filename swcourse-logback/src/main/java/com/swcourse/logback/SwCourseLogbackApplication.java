c94e394c400feb5dbbf08ea63e2869d162ff9933
zhangyuqiang <yuqiang.zhangyq@dtzhejiang.com>
2022-08-25 19:55:38 +0800

    添加log模块

---
+++ swcourse-logback/src/main/java/com/swcourse/logback/SwCourseLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.swcourse.logback;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwCourseLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwCourseLogbackApplication.class, args);
+    }
+}
